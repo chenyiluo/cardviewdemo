a0b384baadcd8b565ae2559c14dd28f364f3eaed
chenyl <chenyl@hongtaiforture.net>
2018-06-05 14:47:50 +0800

    提交

---
+++ app/src/main/java/com/chen/cardviewdemo/MainActivity.java
@@ -0,0 +1,25 @@
+package com.chen.cardviewdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/***说明:
+ * 作者：android
+ * 时间：2018/6/5 14:04
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        /**
+         * CardView使用方法
+         * cardCornerRadius  设置圆角
+         * cardBackgroundColor  设置背景颜色
+         * cardElevation   设置Y轴阴影大小
+         * contentPadding  设置Padding
+         */
+
+    }
+}
